911f20b9c12ad41a2ab734b0923f0f49fb8c9130
GookProg <kalter534226@yandex.ru>
2022-11-08 15:09:10 +0300

    Add circle class

---
+++ Figure/Figures/Circle.cs
@@ -0,0 +1,37 @@
+﻿namespace Figure;
+
+/// <summary>
+/// Provides methods and properties for manage a circle,
+/// such as area calculation.
+/// </summary>
+public class Circle : Figure
+{
+    private double _radius;
+
+    /// <summary>
+    /// Gets the radius of a circle.
+    /// </summary>
+    public double Radius
+    {
+        get => _radius;
+        set
+        {
+            if (value < 0)
+                throw new ArgumentOutOfRangeException("Radius must be non-negative!");
+            _radius = value;
+        }
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the circle.
+    /// </summary>
+    /// <param name="radius"></param>
+    public Circle(double radius) => Radius = radius;
+
+    /// <summary>
+    /// Returns the area of a circle using formula: pi * r^2.
+    /// </summary>
+    /// <returns>Area of a triangle.</returns>
+    protected override double CalculateArea() => Math.PI * Radius * Radius;
+}
+
