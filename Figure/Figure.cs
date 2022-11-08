731126832f5af3254b42de0465caf1442fb9d6a8
GookProg <kalter534226@yandex.ru>
2022-11-08 15:06:11 +0300

    Add figure abstract class

--- Figure/Figure.cs
+++ Figure/Figure.cs
@@ -1,7 +1,18 @@
-﻿namespace Figure
-{
-    public class Figure
-    {
+﻿namespace Figure;
 
-    }
+/// <summary>
+/// Provides the base class for a figure.
+/// </summary>
+public abstract class Figure
+{
+    /// <summary>
+    /// Gets the area of a figure.
+    /// </summary>
+    public double Area => CalculateArea();
+
+    /// <summary>
+    /// Returns the area of a figure.
+    /// </summary>
+    /// <returns>Area of a given figure.</returns>
+    protected abstract double CalculateArea();
 }
