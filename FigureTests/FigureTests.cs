084f0f63ba428f1c2e32d96181350409b360e391
GookProg <kalter534226@yandex.ru>
2022-11-08 15:20:59 +0300

    Add unit tests for figures

---
+++ FigureTests/FigureTests.cs
@@ -0,0 +1,74 @@
+namespace FigureTests;
+
+
+public class FigureTests
+{
+    #region Circle
+
+    /// <summary>
+    /// Test for negative value of a radius.
+    /// </summary>
+    [Test]
+    public void CircleNegativeRadiusTest()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new Circle(-1));
+    }
+
+    /// <summary>
+    /// Test for the area value with correct radius data.
+    /// </summary>
+    [Test]
+    public void CircleCalcSquareTest()
+    {
+        Assert.Multiple(() =>
+        {
+            Assert.That(new Circle(0).Area, Is.EqualTo(0));
+            Assert.That(new Circle(1).Area, Is.EqualTo(Math.PI));
+            Assert.That(new Circle(4).Area, Is.EqualTo(Math.PI * Math.Pow(4, 2)));
+        });
+    }
+
+    #endregion
+
+    #region Triangle
+
+    /// <summary>
+    /// Test on negative side values.
+    /// </summary>
+    [Test]
+    public void TriangleNegativeSidesTest()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new Triangle(-1, 0, 0));
+        Assert.Throws<ArgumentOutOfRangeException>(() => new Triangle(0, -1, 0));
+        Assert.Throws<ArgumentOutOfRangeException>(() => new Triangle(0, 0, -1));
+    }
+
+    /// <summary>
+    /// Test for the area value with correct side values.
+    /// </summary>
+    [Test]
+    public void TriangleCalcSquareTest()
+    {
+        Assert.That(new Triangle(3, 4, 5).Area, Is.EqualTo(6));
+    }
+
+    /// <summary>
+    /// Test for the right triangle.
+    /// </summary>
+    [Test]
+    public void TriangleIsRightAngleTest()
+    {
+        Assert.That(new Triangle(5, 3, 4).IsRight(), Is.EqualTo(true));
+    }
+
+    /// <summary>
+    /// Test for the not right triangle.
+    /// </summary>
+    [Test]
+    public void TriangleIsNotRightAngleTest()
+    {
+        Assert.That(new Triangle(6, 2, 7).IsRight(), Is.EqualTo(false));
+    }
+
+    #endregion
+}
